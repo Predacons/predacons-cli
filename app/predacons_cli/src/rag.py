2b5dce98bf5af0325da23aae92654d479dd511fc
Shourya Shashank <shouryashashank@gmail.com>
2024-10-12 10:47:23 +0530

    Create rag.py

---
+++ app/predacons_cli/src/rag.py
@@ -0,0 +1,90 @@
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain_community.vectorstores import Chroma
+from predacons import PredaconsEmbedding
+
+class VectorStore:
+    def __init__(self, chroma_path, document_path, model = None):
+        self.model_id = model if model is not None else None
+        self.embedder = PredaconsEmbedding(model_id=self.model_id) if self.model_id else PredaconsEmbedding()
+        self.chroma_path = chroma_path
+        self.document_path = document_path
+        
+    
+    def split_text(self,documents: list[Document]):
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=300,
+            chunk_overlap=100,
+            length_function=len,
+            add_start_index=True,
+        )
+        chunks = text_splitter.split_documents(documents)
+        print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+        document = chunks[10]
+        print(document.page_content)
+        print(document.metadata)
+
+        return chunks
+
+    def calculate_chunk_ids(self,chunks):
+
+        # This will create IDs like "data/monopoly.pdf:6:2"
+        # Page Source : Page Number : Chunk Index
+
+        last_page_id = None
+        current_chunk_index = 0
+
+        for chunk in chunks:
+            source = chunk.metadata.get("source")
+            page = chunk.metadata.get("page")
+            current_page_id = f"{source}:{page}"
+
+            # If the page ID is the same as the last one, increment the index.
+            if current_page_id == last_page_id:
+                current_chunk_index += 1
+            else:
+                current_chunk_index = 0
+
+            # Calculate the chunk ID.
+            chunk_id = f"{current_page_id}:{current_chunk_index}"
+            last_page_id = current_page_id
+
+            # Add it to the page meta-data.
+            chunk.metadata["id"] = chunk_id
+
+        return chunks
+
+    def add_to_chroma(self,chunks: list[Document]):
+        # Load the existing database.
+        db = Chroma(
+            persist_directory=self.chroma_path, embedding_function=self.embedder
+        )
+
+        # Calculate Page IDs.
+        chunks_with_ids = self.alculate_chunk_ids(chunks)
+
+        # Add or Update the documents.
+        existing_items = db.get(include=[])  # IDs are always included by default
+        existing_ids = set(existing_items["ids"])
+        print(f"Number of existing documents in DB: {len(existing_ids)}")
+
+        # Only add documents that don't exist in the DB.
+        new_chunks = []
+        for chunk in chunks_with_ids:
+            if chunk.metadata["id"] not in existing_ids:
+                new_chunks.append(chunk)
+
+        if len(new_chunks):
+            print(f"👉 Adding new documents: {len(new_chunks)}")
+            new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+            db.add_documents(new_chunks, ids=new_chunk_ids)
+            db.persist()
+        else:
+            print("✅ No new documents to add")
+
+    def load_documents(self):
+        loader = DirectoryLoader(self.document_path)
+        documents = loader.load()
+        return documents
